83d70dc9f8074dc751c57dbc8fc91de658353cec
sergeykochiev <sergeyivanovo547@gmail.com>
2024-05-28 15:59:55 +0300

    asdpiohoasihd

--- client/src/components/Select.tsx
+++ client/src/components/Select.tsx
@@ -1,4 +1,6 @@
-import { SelectHTMLAttributes } from 'react'
+import { SelectHTMLAttributes, useEffect, useState } from 'react'
+import { ModelUnion } from '../types'
+import fetchApi from '../helpers/fetchApi'
 
 export default function Select(p: SelectHTMLAttributes<HTMLSelectElement>) {
     return (
@@ -7,3 +9,24 @@ export default function Select(p: SelectHTMLAttributes<HTMLSelectElement>) {
         </select>
     )
 }
+
+interface ForeignSelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+    target: string
+}
+
+export function ForeignSelect<T extends ModelUnion>(p: ForeignSelectProps) {
+    const [data, setData] = useState<T[]>()
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetchApi("GET", p.target)
+            setData((await data.json()).results)
+        }
+        fetchData()
+    }, [])
+    return (
+        <select className="transition-all w-full bg-white p-2 px-3 flex-1 rounded-xl focus:bg-fav font-bold text-slate-500 focus:text-white outline outline-2 -outline-offset-2 hover:outline-fav outline-transparent" {...p}>
+            {data && Object.values(data).map(e => <option value={e.id}>{e.name || e.fio}</option>)}
+        </select>
+    )
+}
+
--- client/src/helpers/getFields.tsx
+++ client/src/helpers/getFields.tsx
@@ -1,8 +1,9 @@
-import Select from "../components/Select";
+import Select, { ForeignSelect } from "../components/Select";
 import TextInput from "../components/TextInput";
 import VariantInput from "../components/VariantInput";
 import Category, { CategoryKey } from "../model/Category";
 import { AnyTableKey, ModelUnion } from "../types";
+import fetchApi from "./fetchApi";
 
 export default function getFields(table: AnyTableKey, init?: ModelUnion) {
     const get = (field: keyof ModelUnion): string | undefined => init && init[field] as string
@@ -11,7 +12,7 @@ export default function getFields(table: AnyTableKey, init?: ModelUnion) {
             return <>
                 <TextInput required name="fio" type="text" placeholder="ФИО" defaultValue={get("fio")}/>
                 <TextInput required name="phone_number" type="tel" placeholder="Номер" defaultValue={get("phone_number")}/>
-                <Select defaultValue={get("category")} name="category">
+                <Select defaultValue={get("category")} name="category" required>
                     {Object.keys(Category).map(key => <option key={key} value={key}>{Category[key as CategoryKey]}</option>)}
                 </Select>
                 <TextInput required name="email" type="email" placeholder="Эл. почта" defaultValue={get("email")}/>
@@ -24,8 +25,8 @@ export default function getFields(table: AnyTableKey, init?: ModelUnion) {
             </>
         case "visit":
             return <>
-                <TextInput required name="client_id" type="number" placeholder="ID клиента" defaultValue={get("client_id")}/>
-                <TextInput required name="service_id" type="number" placeholder="ID сервиса" defaultValue={get("service_id")}/>
+                <ForeignSelect target="client" name="client_id" defaultValue={get("client_id")}/>
+                <ForeignSelect target="service" name="service_id" defaultValue={get("service_id")}/>
                 <TextInput required name="date" type="date" placeholder="Дата" defaultValue={get("date")}/>
                 <TextInput required name="time" type="time" placeholder="Время" defaultValue={get("time")}/>
                 <VariantInput name="servic_fullfilled" label="Услуга оказана?" defaultTrue={init ? init.service_fullfilled as boolean : true}/>
